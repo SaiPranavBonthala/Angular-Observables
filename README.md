289bf2740dc92170b63c02fd5f3da38d1a3b4f50
SaiPranavBonthala <pranavraj2097@gmail.com>
2021-03-07 18:52:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-iwaqia
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-iwaqia)
---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, VERSION } from "@angular/core";
+import { Observable, Subscription } from "rxjs";
+import { map } from "rxjs/operators";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  name = "Angular " + VERSION.major;
+  private firstObsSubscription: Subscription;
+
+  constructor() {}
+
+  ngOnInit() {
+    const customObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error("Count is greater than 3"));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customObservable
+      .pipe(
+        map((data: number) => {
+          return "Round: " + (data + 1);
+        })
+      )
+      .subscribe(
+        data => {
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log("Completed");
+        }
+      );
+  }
+}
